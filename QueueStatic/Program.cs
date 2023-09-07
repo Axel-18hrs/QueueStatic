ecb3d8c99a11a95d08481b4e5ba1782b75bdd377
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-09-06 22:55:31 -0600

    Agregar archivos de proyecto.

---
+++ QueueStatic/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueExample cola = new QueueExample(5);
+            bool salir = true;
+
+            while (salir)
+            {
+                Console.WriteLine("// Select an option" +
+                    "\n[1]: Enter a value " +
+                    "\n[2]: Delete first element" +
+                    "\n[3]: Show first element" +
+                    "\n[4]: Show Queue" +
+                    "\n[5]: Exit");
+
+                int opcion;
+
+                try
+                {
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (opcion)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter a value:");
+                        cola.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        if (!cola.IsEmpty())
+                        {
+                            int element = cola.Dequeue();
+                            Console.Clear();
+                            Console.WriteLine("// Element dequeued from the queue: " + element);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The Queue is empty");
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        if (!cola.IsEmpty())
+                        {
+
+                            int elementoDs = cola.Peek();
+                            Console.Clear();
+                            Console.WriteLine("// Element Peeked from the queue: " + elementoDs);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The Queue is empty");
+                        }
+
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        cola.DisplayQueue();
+
+                        break;
+
+                    case 5:
+                        salir = false;
+                        Console.Clear();
+                        Console.WriteLine("Good bye");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ QueueStatic/QueueExample.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueStatic
+{
+    class QueueExample
+    {
+        private int maxSize; // Tamaño máximo de la cola
+        private int front;    // Índice del elemento frontal
+        private int rear;     // Índice del elemento posterior
+        private int[] queue;  // Arreglo para almacenar elementos
+
+        public QueueExample(int size)
+        {
+            maxSize = size;
+            front = 0;
+            rear = -1;
+            queue = new int[maxSize];
+        }
+
+        public void Enqueue(int item)
+        {
+            if (rear < maxSize - 1)
+            {
+                queue[++rear] = item;
+            }
+            else
+            {
+                Console.WriteLine("La cola está llena. No se puede agregar más elementos.");
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (front <= rear)
+            {
+                return queue[front++];
+            }
+            else
+            {
+                Console.WriteLine("La cola está vacía. No se puede sacar ningún elemento.");
+                return -1;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return front > rear;
+        }
+
+        public int Peek()
+        {
+            if (!IsEmpty())
+            {
+                return queue[front];
+            }
+            else
+            {
+                Console.WriteLine("La cola está vacía. No se puede consultar el elemento frontal.");
+                return -1;
+            }
+        }
+        public void DisplayQueue()
+        {
+            if (!IsEmpty())
+            {
+                Console.WriteLine("Queue elements:");
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The Queue is empty");
+            }
+        }
+    }
+}
