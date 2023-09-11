51b591fcb97250ae5d71acafd22ea29be085b5ef
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-09-10 21:32:11 -0600

    new

--- QueueStatic/Program.cs
+++ QueueStatic/Program.cs
@@ -15,6 +15,7 @@ namespace QueueStatic
 
             while (salir)
             {
+                Console.Clear();
                 Console.WriteLine("// Select an option" +
                     "\n[1]: Enter a value " +
                     "\n[2]: Delete first element" +
@@ -27,70 +28,70 @@ namespace QueueStatic
                 try
                 {
                     opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Enter a value:");
+                            cola.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            if (!cola.IsEmpty())
+                            {
+                                int element = cola.Dequeue();
+                                Console.Clear();
+                                Console.WriteLine("// Element dequeued from the queue: " + element);
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("The Queue is empty");
+                            }
+
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            if (!cola.IsEmpty())
+                            {
+
+                                int elementoDs = cola.Peek();
+                                Console.Clear();
+                                Console.WriteLine("// Element Peeked from the queue: " + elementoDs);
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("The Queue is empty");
+                            }
+
+                            break;
+
+                        case 4:
+                            Console.Clear();
+                            cola.DisplayQueue();
+
+                            break;
+
+                        case 5:
+                            salir = false;
+                            Console.Clear();
+                            Console.WriteLine("Good bye");
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
                     Console.Clear();
-                    continue;
-                }
-
-                switch (opcion)
-                {
-
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("Enter a value:");
-                        cola.Enqueue(Convert.ToInt32(Console.ReadLine()));
-                        break;
-
-                    case 2:
-                        Console.Clear();
-                        if (!cola.IsEmpty())
-                        {
-                            int element = cola.Dequeue();
-                            Console.Clear();
-                            Console.WriteLine("// Element dequeued from the queue: " + element);
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("The Queue is empty");
-                        }
-
-                        break;
-
-                    case 3:
-                        Console.Clear();
-                        if (!cola.IsEmpty())
-                        {
-
-                            int elementoDs = cola.Peek();
-                            Console.Clear();
-                            Console.WriteLine("// Element Peeked from the queue: " + elementoDs);
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("The Queue is empty");
-                        }
-
-                        break;
-
-                    case 4:
-                        Console.Clear();
-                        cola.DisplayQueue();
-
-                        break;
-
-                    case 5:
-                        salir = false;
-                        Console.Clear();
-                        Console.WriteLine("Good bye");
-                        break;
+                    Console.WriteLine("// Not a valid choice");
                 }
 
                 Console.ReadKey();
-                Console.Clear();
+               
             }
         }
     }
